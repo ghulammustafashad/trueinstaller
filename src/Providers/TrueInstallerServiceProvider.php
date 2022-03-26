7452d1e928719958690d10d26059138ac5a60233
Ghulam Mustafa Shad <ghulamshad@gmail.com>
2022-03-26 18:00:08 +0500

    Update TrueInstallerServiceProvider.php

--- src/Providers/TrueInstallerServiceProvider.php
+++ src/Providers/TrueInstallerServiceProvider.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace GhulamMustafaShad\TrueInstaller\Providers;
+namespace ghulammustafashad\trueinstaller\Providers;
 
 use Illuminate\Routing\Router;
 use Illuminate\Support\ServiceProvider;
-use GhulamMustafaShad\TrueInstaller\Middleware\canInstall;
-use GhulamMustafaShad\TrueInstaller\Middleware\canUpdate;
+use ghulammustafashad\trueinstaller\Middleware\canInstall;
+use ghulammustafashad\trueinstaller\Middleware\canUpdate;
 
 class TrueInstallerServiceProvider extends ServiceProvider
 {
@@ -46,19 +46,19 @@ class TrueInstallerServiceProvider extends ServiceProvider
     protected function publishFiles()
     {
         $this->publishes([
-            __DIR__.'/../Config/installer.php' => base_path('config/installer.php'),
-        ], 'laravelinstaller');
+            __DIR__.'/../Config/trueinstaller.php' => base_path('config/trueinstaller.php'),
+        ], 'trueinstaller');
 
         $this->publishes([
-            __DIR__.'/../assets' => public_path('installer'),
-        ], 'laravelinstaller');
+            __DIR__.'/../assets' => public_path('trueinstaller'),
+        ], 'trueinstaller');
 
         $this->publishes([
-            __DIR__.'/../Views' => base_path('resources/views/vendor/installer'),
-        ], 'laravelinstaller');
+            __DIR__.'/../Views' => base_path('resources/views/trueinstaller'),
+        ], 'trueinstaller');
 
         $this->publishes([
             __DIR__.'/../Lang' => base_path('resources/lang'),
-        ], 'laravelinstaller');
+        ], 'trueinstaller');
     }
 }
