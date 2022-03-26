7695e6a8f0bcff680ff17f34f52b2e339f8d2cb6
Ghulam Mustafa Shad <ghulamshad@gmail.com>
2022-03-26 18:03:43 +0500

    Update FinalController.php

--- src/Controllers/FinalController.php
+++ src/Controllers/FinalController.php
@@ -1,21 +1,21 @@
 <?php
 
-namespace GhulamMustafaShad\TrueInstaller\Controllers;
+namespace ghulammustafashad\trueinstaller\Controllers;
 
 use Illuminate\Routing\Controller;
-use GhulamMustafaShad\TrueInstaller\Events\LaravelInstallerFinished;
-use GhulamMustafaShad\TrueInstaller\Helpers\EnvironmentManager;
-use GhulamMustafaShad\TrueInstaller\Helpers\FinalInstallManager;
-use GhulamMustafaShad\TrueInstaller\Helpers\InstalledFileManager;
+use ghulammustafashad\trueinstaller\Events\LaravelInstallerFinished;
+use ghulammustafashad\trueinstaller\Helpers\EnvironmentManager;
+use ghulammustafashad\trueinstaller\Helpers\FinalInstallManager;
+use ghulammustafashad\trueinstaller\Helpers\InstalledFileManager;
 
 class FinalController extends Controller
 {
     /**
      * Update installed file and display finished view.
      *
-     * @param \GhulamMustafaShad\TrueInstaller\Helpers\InstalledFileManager $fileManager
-     * @param \GhulamMustafaShad\TrueInstaller\Helpers\FinalInstallManager $finalInstall
-     * @param \GhulamMustafaShad\TrueInstaller\Helpers\EnvironmentManager $environment
+     * @param \ghulammustafashad\trueinstaller\Helpers\InstalledFileManager $fileManager
+     * @param \ghulammustafashad\trueinstaller\Helpers\FinalInstallManager $finalInstall
+     * @param \ghulammustafashad\trueinstaller\Helpers\EnvironmentManager $environment
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function finish(InstalledFileManager $fileManager, FinalInstallManager $finalInstall, EnvironmentManager $environment)
